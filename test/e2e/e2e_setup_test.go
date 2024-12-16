35522c0577f84ad7aa3d50f0010ee34615d5f592
Christian Schlotter <christian.schlotter@broadcom.com>
2024-12-16 16:53:27 +0100

    increase timeouts

--- test/e2e/e2e_setup_test.go
+++ test/e2e/e2e_setup_test.go
@@ -304,7 +304,7 @@ func addVCSimTestVariables(managementClusterProxy framework.ClusterProxy, specNa
 			return false
 		}
 		return len(envVar.Status.Variables) > 0
-	}, 30*time.Second, 5*time.Second).Should(BeTrue(), "Failed to get EnvVar %s", klog.KObj(envVar))
+	}, 300*time.Second, 5*time.Second).Should(BeTrue(), "Failed to get EnvVar %s", klog.KObj(envVar))
 
 	Byf("Setting test variables for %s", specName)
 	for k, v := range envVar.Status.Variables {
@@ -351,7 +351,7 @@ func setupNamespaceWithVMOperatorDependenciesVCSim(managementClusterProxy framew
 			return false
 		}
 		return dependenciesConfig.Status.Ready
-	}, 30*time.Second, 5*time.Second).Should(BeTrue(), "Failed to get VMOperatorDependencies on namespace %s", workloadClusterNamespace)
+	}, 300*time.Second, 5*time.Second).Should(BeTrue(), "Failed to get VMOperatorDependencies on namespace %s", workloadClusterNamespace)
 }
 
 func setupNamespaceWithVMOperatorDependenciesVCenter(managementClusterProxy framework.ClusterProxy, workloadClusterNamespace string) {
